e95f03989a72a3d3272360d0bd0a4d224e413152
WilliamX <williamx@deskangel.com>
2020-06-16 15:54:39 +0800

    * update

--- android/src/main/java/sq/flutter/ssh/SshPlugin.java
+++ android/src/main/java/sq/flutter/ssh/SshPlugin.java
@@ -311,7 +311,7 @@ public class SshPlugin implements MethodCallHandler, StreamHandler {
       }
     }).start();
   }
-  
+
   private void portForwardL(final HashMap args, final Result result) {
     new Thread(new Runnable()  {
       public void run() {
@@ -319,13 +319,13 @@ public class SshPlugin implements MethodCallHandler, StreamHandler {
           SSHClient client = getClient(args.get("id").toString(), result);
           if (client == null)
             return;
-          
+
           Session session = client._session;
           int rport = Integer.parseInt(args.get("rport").toString());
           int lport = Integer.parseInt(args.get("lport").toString());
           String rhost = args.get("rhost").toString();
           int assinged_port=session.setPortForwardingL(lport, rhost, rport);
-          
+
           result.success(Integer.toString(assinged_port));
         } catch (JSchException error) {
           Log.e(LOGTAG, "Error connecting portforwardL:" + error.getMessage());
